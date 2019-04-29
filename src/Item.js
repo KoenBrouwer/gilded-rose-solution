e2429b4d659aa48494c91e2414f9389725006096
Koen <>
2019-04-29 15:25:46 +0200

    Added some JSDoc.

--- src/Item.js
+++ src/Item.js
@@ -1,5 +1,12 @@
 class Item {
-	constructor(name, sellIn, quality){
+
+	/**
+	 * Item
+	 * @param {string} name Name of the product
+	 * @param {int} sellIn Number of days in which this Item should be sold
+	 * @param {int} quality Describes how valuable this Item is
+	 */
+	constructor(name, sellIn, quality) {
 		this.name = name;
 		this.sellIn = sellIn;
 		this.quality = quality;
